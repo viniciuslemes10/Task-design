67a6f2b32a84fdb7a8a8dc922e3325db12428ce5
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-05-26 09:18:44 -0300

    Creating repository for tasks

---
+++ src/main/java/br/com/tak/repositories/TaskRepository.java
@@ -0,0 +1,9 @@
+package br.com.tak.repositories;
+
+import br.com.tak.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
